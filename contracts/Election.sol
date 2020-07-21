78f563355bb743708a58429522b3fa7e5e3e6051
smalldutta <smalldutta@gmail.com>
2020-07-22 01:08:13 +0300

    Added tests and updated gitignore

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,8 +2,29 @@ pragma solidity ^0.5.16;
 
 contract Election{
 
-    string public candidate;
+    //Model a candidate
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    //Store candidates
+    //Fetch a candidate
+    mapping(uint=> Candidate) public candidates;
+
+    //Store a candidate's count
+    uint public candidatesCount=0;
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+
+
     constructor () public{
-            candidate = "Candidate";
+        addCandidate("Candidate-1");
+        addCandidate("Candidate-2");
     }
 }
