d7d13cad1c1ecbe820f88ecfdab7b27b3e01a429
smalldutta <smalldutta@gmail.com>
2020-07-22 12:41:39 +0300

    Added event listeners and enhanced voting

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -13,12 +13,37 @@ contract Election{
     //Fetch a candidate
     mapping(uint=> Candidate) public candidates;
 
+    //Store accounts that have voted
+    mapping(address=> bool) public voters;
+
     //Store a candidate's count
     uint public candidatesCount=0;
+
+    //voted event
+    event votedEvent(
+        uint indexed _candidateId
+    );
 
     function addCandidate(string memory _name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        //require that they havent voted before
+        require(!voters[msg.sender]);
+
+        //require a valid candidate
+        require(_candidateId>0&& _candidateId<= candidatesCount);
+
+        //update candidate vote count
+        candidates[_candidateId].voteCount++;
+        
+        //record that voter has voted
+        voters[msg.sender] = true;
+
+        //trigger voted event
+        emit  votedEvent(_candidateId);
     }
 
 
